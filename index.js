e0bb41c998ffb06a8dd0038b92be58663adc613b
EkramH <ekramhosen329@gmail.com>
2022-05-23 11:45:17 +0600

    get products api

--- index.js
+++ index.js
@@ -4,8 +4,36 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ijdbv.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("handy_data").collection("products");
+
+    // Get all products
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Handy Portal Sever On!!!!");
